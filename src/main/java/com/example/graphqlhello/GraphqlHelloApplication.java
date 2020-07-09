22dd403168e7686c76a0423b2bf2aeb77090ab73
唐博 <tangbo@baijiahulian.com>
2020-07-09 19:24:31 +0800

    framework

---
+++ src/main/java/com/example/graphqlhello/GraphqlHelloApplication.java
@@ -0,0 +1,13 @@
+package com.example.graphqlhello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlHelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlHelloApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/graphqlhello/GraphqlHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.graphqlhello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlHelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
